cf41a1d612931930f50f2e4f3b5f4a4d74e9806a
Vivek Singh <vivekonline01@gmail.com>
2018-09-14 15:48:29 +0530

    code refactored into containers and components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Sidebar from './Sidebar';
-import Main from './Main';
+import Sidebar from '../components/Sidebar';
+import Main from '../components/Main';
 import './App.css';
-import store from './store';
+import store from '../store';
 import _ from 'lodash';
 
 class App extends Component {
